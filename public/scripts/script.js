4e91273938d5f18b4ebb11c9ccf78b6745ded133
HyeokjinKang <coupy@croissant.tech>
2020-11-28 00:29:20 +0900

    updated strings

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,16 +1,16 @@
 let typed = new Typed('#typeText', {
   strings: [
-    'special',
-    'awesome',
-    'adorable',
-    'elegant',
-    'beautiful',
-    'wonderful',
+    'special&#9996;',
+    'awesome&#128170;',
+    'adorable&#128152;',
+    'elegant&#128141;',
+    'beautiful&#10024;',
+    'wonderful&#127752;',
     'lit&#128293;'
   ],
-  typeSpeed: 70,
+  typeSpeed: 60,
   shuffle: true,
-  backSpeed: 50,
+  backSpeed: 40,
   backDelay: 2000,
   loop: true,
   loopCount: Infinity
