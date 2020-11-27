7f4ade897e97adec6974d37fb0edaf6dcc0d1375
HyeokjinKang <coupy@croissant.tech>
2020-11-27 23:43:42 +0900

    initial setup

---
+++ public/scripts/script.js
@@ -0,0 +1,13 @@
+let typed = new Typed('#typeText', {
+  strings: [
+    'special',
+    'awesome',
+    'adorable',
+    'elegant'
+  ],
+  typeSpeed: 70,
+  backSpeed: 50,
+  backDelay: 2000,
+  loop: true,
+  loopCount: Infinity
+});
